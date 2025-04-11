01ec9eb7de288b170873645c4b23853f2229a115
syahirghariff <syahirghariff@gmail.com>
2025-04-11 23:01:21 +0800

    Project initiation 🚀

---
+++ src/main/java/com/devsy/statementprocessorservice/pojo/Statement.java
@@ -0,0 +1,17 @@
+package com.devsy.statementprocessorservice.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class Statement {
+    private String date;
+    private String description;
+    private String amount;
+}
---
+++ src/main/java/com/devsy/statementprocessorservice/reader/StatementReader.java
@@ -0,0 +1,7 @@
+package com.devsy.statementprocessorservice.reader;
+
+import java.io.File;
+
+public interface StatementReader {
+    void read(File file);
+}
