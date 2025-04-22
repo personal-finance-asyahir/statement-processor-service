0a19cce168b9d62b0d38c88296b1c9b79a9c69ac
syahirghariff <syahirghariff@gmail.com>
2025-04-22 17:14:19 +0800

    Batch job config configuration

--- src/main/java/com/asyahir/statementprocessorservice/batch/MaybankDebitBatchConfiguration.java
+++ src/main/java/com/asyahir/statementprocessorservice/batch/MaybankDebitBatchConfiguration.java
@@ -4,16 +4,27 @@ import com.asyahir.statementprocessorservice.entity.Transaction;
 import com.asyahir.statementprocessorservice.pojo.MaybankDebit;
 import com.asyahir.statementprocessorservice.processor.MaybankDebitItemProcessor;
 import com.asyahir.statementprocessorservice.reader.MaybankDebitItemReader;
+import com.asyahir.statementprocessorservice.repository.TransactionRepository;
+import com.asyahir.statementprocessorservice.writer.MaybankDebitItemWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import java.io.IOException;
 
 @Configuration
+@EnableBatchProcessing
 public class MaybankDebitBatchConfiguration {
 
     @Bean
@@ -27,7 +38,31 @@ public class MaybankDebitBatchConfiguration {
         return new MaybankDebitItemProcessor();
     }
 
+    @Bean
+    public ItemWriter<Transaction> writer(TransactionRepository transactionRepository) {
+        return new MaybankDebitItemWriter(transactionRepository);
+    }
 
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      DataSourceTransactionManager transactionManager,
+                      ItemReader<MaybankDebit> reader,
+                      ItemProcessor<MaybankDebit, Transaction> processor,
+                      ItemWriter<Transaction> writer){
 
+        return new StepBuilder("maybankDebitStep1", jobRepository)
+                .<MaybankDebit, Transaction>chunk(5, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public Job maybankDebitJob (JobRepository jobRepository, Step step1) {
+        return new JobBuilder("maybankDebitJob", jobRepository)
+                .start(step1)
+                .build();
+    }
 
 }
