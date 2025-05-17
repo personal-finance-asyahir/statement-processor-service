d885a415b51d57f69ffd052ba9af810e006eea3b
syahirghariff <syahirghariff@gmail.com>
2025-05-17 15:07:59 +0800

    ItemProcessor for credit card information

--- src/main/java/com/asyahir/statementprocessorservice/processor/MaybankCreditItemProcessor.java
+++ src/main/java/com/asyahir/statementprocessorservice/processor/MaybankCreditItemProcessor.java
@@ -2,7 +2,15 @@ package com.asyahir.statementprocessorservice.processor;
 
 import com.asyahir.statementprocessorservice.entity.MaybankCredit;
 import com.asyahir.statementprocessorservice.pojo.MaybankCreditData;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class MaybankCreditItemProcessor implements ItemProcessor<MaybankCreditData, MaybankCredit> {
 
@@ -13,7 +21,36 @@ public class MaybankCreditItemProcessor implements ItemProcessor<MaybankCreditDa
     }
 
     @Override
-    public MaybankCredit process(MaybankCreditData item) throws Exception {
-        return null;
+    public MaybankCredit process(@NonNull MaybankCreditData item) throws Exception {
+
+        char operation = '-';
+
+        String amount = item.getAmount();
+        if (StringUtils.contains(amount, "CR")) {
+            operation = '+';
+            amount = StringUtils.replace(amount, "CR", "");
+        }
+
+        // Statement Date Year
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yy");
+        LocalDate statementDate = LocalDate.parse(item.getStatementDate(), formatter);
+
+        // Posting Date
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/MM");
+        MonthDay postDateDay = MonthDay.parse(item.getPostingDate(), formatter2);
+        LocalDate postDateYear = LocalDate.of(statementDate.getYear(), postDateDay.getMonth(), postDateDay.getDayOfMonth());
+
+        // Transaction Date
+        MonthDay trxDateDay = MonthDay.parse(item.getDate(), formatter2);
+        LocalDate trxDateYear = LocalDate.of(statementDate.getYear(), trxDateDay.getMonth(), trxDateDay.getDayOfMonth());
+
+        return MaybankCredit.builder()
+                .userId(UUID.fromString(this.userId))
+                .description(StringUtils.trim(item.getDescription()))
+                .operation(operation)
+                .amount(Double.parseDouble(amount))
+                .postingDate(postDateYear)
+                .transactionDate(trxDateYear)
+                .insertedDateTime(LocalDateTime.now()).build();
     }
 }
